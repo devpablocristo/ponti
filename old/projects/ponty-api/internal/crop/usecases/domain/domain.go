81b60319611f2224f1f846286b008707aa9ae690
devpablocristo <devpablocristo@gmail.com>
2025-05-15 15:13:28 -0300

    old borrado

--- old/projects/ponty-api/internal/crop/usecases/domain/domain.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-// Crop (Cultivo) represents the crop domain entity.
-type Crop struct {
-	ID   int64
-	Name string
-}
--- old/projects/ponty-api/internal/field/handler/dto/update_field.go
+++
@@ -1,7 +0,0 @@
-// File: internal/field/handler/dto/update_field.go
-package dto
-
-// UpdateFieldRequest is the payload for updating a Field.
-type UpdateField struct {
-	Field
-}
